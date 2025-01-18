60f8fa4d9ca4e22327ef2ec039edeb609c2f970c
OwenxOwen <oreynold@ashland.edu>
2025-01-18 13:01:24 -0500

    Cleared Boilerplate

--- frontend/church-app/src/App.jsx
+++ frontend/church-app/src/App.jsx
@@ -10,26 +10,7 @@ function App() {
 
   return (
     <>
-      <motion.div animate={{ rotate: 360, x:200 }} transition={{ duration: 2, ease:"easeOut" }} whileHover={{x:-200}}>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </motion.div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test</h1>
     </>
   )
 }
