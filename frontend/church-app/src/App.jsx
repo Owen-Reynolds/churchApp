bf5e1500385443eeda355cca24e4d6786c5b72d8
OwenxOwen <oreynold@ashland.edu>
2025-01-19 09:02:40 -0500

    Created Header

--- frontend/church-app/src/App.jsx
+++ frontend/church-app/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import {easeOut, motion} from 'framer-motion';
+import Header from './components/Header/Header';
+
+import {motion} from 'framer-motion';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Test</h1>
