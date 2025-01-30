824c8877fcda87f82b8408db963b426896f9be1f
Owen-Reynolds <oreynold@ashland.edu>
2025-01-29 20:52:38 -0500

    Started Connect Page

--- frontend/church-app/src/pages/Connect/Connect.jsx
+++ frontend/church-app/src/pages/Connect/Connect.jsx
@@ -3,7 +3,32 @@ import './Connect.css';
 export default function Connect() {
     return(
     <>
-        <h1>Connect Page</h1>
+        <div className="connectContent">
+            <div className="connectText">
+                <h1>Contact Us</h1>
+                <p>Have a question or need help? We would love to hear from you. Fill out the form below and we will get back to you as soon as possible.</p>
+                <div className="contactBox">
+                    <div className="phoneBox">
+                        <img src="" alt="" />
+                        <p>(440)-290-0233</p>
+                    </div>
+                    <div className="locationBox">
+                        <img src="" alt="" />
+                        <p>Address goes here...</p>
+                    </div>
+                </div>
+            </div>
+            <div className="connectForm">
+                <form action="">
+                    <div className="nameBox">
+                        <label htmlFor="">Name</label>
+                        <div className="nameBox"></div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        
+
     </>
     );
 }
