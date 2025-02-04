ce67f6d4f31ca135228b9666a346fd379815a5cf
OwenxOwen <oreynold@ashland.edu>
2025-02-04 11:06:25 -0500

    Started Connect Page

--- frontend/church-app/src/pages/Connect/Connect.jsx
+++ frontend/church-app/src/pages/Connect/Connect.jsx
@@ -1,29 +1,44 @@
 import './Connect.css';
 
+import { motion } from 'framer-motion';
+import crossImage from '../../assets/vitalitylogoblue.png';
+
 export default function Connect() {
     return(
     <>
-        <div className="connectContent">
+        <div className="connectContent" style={{backgroundImage: `url(${crossImage})`}}>
             <div className="connectText">
-                <h1>Contact Us</h1>
-                <p>Have a question or need help? We would love to hear from you. Fill out the form below and we will get back to you as soon as possible.</p>
-                <div className="contactBox">
-                    <div className="phoneBox">
-                        <img src="" alt="" />
-                        <p>(440)-290-0233</p>
-                    </div>
-                    <div className="locationBox">
-                        <img src="" alt="" />
-                        <p>Address goes here...</p>
-                    </div>
+                <div className="connectTitle">
+                    <h1>Connect With <b>US.</b></h1>
+                </div>
+                <div className="connectSubtitle">
+                    <p>Fill out the form to get in touch with us. 
+                    Responses are usually handled within 48 hours. </p>
                 </div>
             </div>
             <div className="connectForm">
-                <form action="">
+                <form action="">    
+                    <label htmlFor="">Name</label>
                     <div className="nameBox">
-                        <label htmlFor="">Name</label>
-                        <div className="nameBox"></div>
+                        <input type="text" placeholder="First" />
+                        <input type="text" placeholder="Last" />
                     </div>
+                    <label htmlFor="">Email</label>
+                    <div className="emailBox">
+                        <input type="email" placeholder="example@email.com" />
+                    </div>
+                    <label htmlFor="">Message</label>
+                    <div className="messageBox">
+                        <textarea name="" id="" cols={5}></textarea>
+                    </div>
+                    <div className="checkBox">
+                        <input type="checkbox" />
+                        <label htmlFor="">I would like to enroll in the Vitality Church Email List.</label>
+                    </div>
+                    <motion.button
+                    whileHover={{ scale: 1.05}}
+                    whileInView={{ scale: 0.9}}
+                    type="submit">Send</motion.button>
                 </form>
             </div>
         </div>
