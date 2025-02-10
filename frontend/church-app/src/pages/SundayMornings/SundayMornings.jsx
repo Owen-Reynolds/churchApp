96977012611248a9438a306be3ad313694b631e3
OwenxOwen <oreynold@ashland.edu>
2025-02-09 22:09:37 -0500

    Added Background, Started Boxes

--- frontend/church-app/src/pages/SundayMornings/SundayMornings.jsx
+++ frontend/church-app/src/pages/SundayMornings/SundayMornings.jsx
@@ -1,48 +1,116 @@
 import './SundayMornings.css';
-import React,  {useState} from 'react';
-import {motion} from 'framer-motion';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 
+import heartRate from '../../assets/connectAssets/heartrate.png';
 import biblepicture from '../../assets/sundayMorningAssets/biblepicture.jpg';
 import learnPhoto from '../../assets/sundayMorningAssets/learnPhoto.jpg';
 import coffeePhoto from '../../assets/sundayMorningAssets/coffeePhoto.jpg';
 import clockPhoto from '../../assets/sundayMorningAssets/clockPhoto.jpg';
 
 export default function SundayMornings() {
-    return(
-    <>
-        <div className="titleSection">
-            <img src="" alt="" />
-            <div className="titleSectionContent">
-                <h1>Your welcome here.</h1>
-                <p>At Vitality church...</p>
+    const [expandedCard, setExpandedCard] = useState(null);
+
+    return (
+        <>
+            <div className="titleSection">
+                <h1>
+                    <div>We Gladly</div>
+                    <div>Welcome You</div>
+                </h1>
+                <p>Learn more below</p>
+                <i className="downArrow"></i>
             </div>
-        </div>
-        <div className="cardSection">
-            <Card image={biblepicture} title="Should I Bring A Bible?" description="Bring your Bible and if you don't have one let us know and we will provide one for you to keep. Don't worry, scriptures will be displayed on the screen but our goal is to have everyone in the Bible following along.  Please, don't feel inadequate if you don't know your way through Bible yet, truth is no one has! Everyone is learning!" />
-            <Card image={learnPhoto} title="What Will Be Taught?" description="Together we will guide each other through God's life changing truths. Remember, our messages are Biblical.  Each week we take what the Bible says and learn how to apply it to our real life issues and questions and purpose.  God's Word grows us and feeds our souls, God gives you something through His Word on Sunday that you can start applying on Sunday. "/>
-            <Card image={clockPhoto} title="How Long Is Service?" description="We meet every Sunday at 10:00 AM.  We are located at 1000 N. 16th St. in Grand Junction, CO.  We would love to have you join us!"/>
-            <Card image={coffeePhoto} title="After Service?" description="We provide great coffee and refreshments after each service in our fellowship hall for your enjoyment and relationship building we value our children and they have fun during this time just being kids. This a huge part of who we are as a church, and we would be blessed to connect join in the joy."/>
-        </div>
-    </>
+            <div className="welcomeSection">
+                <div className="welcomeContent">
+                    <motion.h2
+                    initial={{opacity: 0, y: -300}}
+                    whileInView={{opacity: 1, y: 0}}
+                    transition={{duration: 2.0}}    
+                    >We Want You To Feel</motion.h2>    
+                    <div className="welcomeWords">
+                        <motion.h2
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        transition={{duration: 1.0}}>
+                        Loved.
+                        </motion.h2>
+                        <motion.h2
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        transition={{duration: 2.0}}>
+                        Heard.
+                        </motion.h2>
+                        <motion.h2
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        transition={{duration: 3.0}}
+                        >Valued.
+                        </motion.h2>
+                        <motion.h2
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        transition={{duration: 4.0}}
+                        >Accepted.
+                        </motion.h2>
+                        <motion.h2
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        transition={{duration: 5.0}}>
+                        Welcomed.
+                        </motion.h2>
+                    </div>
+                </div>
+            </div>
+            <div className="cardSection">
+                <Card
+                    image={biblepicture}
+                    title="Should I Bring A Bible?"
+                    description="Bring your Bible and if you don't have one let us know and we will provide one for you to keep. Don't worry, scriptures will be displayed on the screen but our goal is to have everyone in the Bible following along.  Please, don't feel inadequate if you don't know your way through Bible yet, truth is no one has! Everyone is learning!"
+                    isExpanded={expandedCard === 1}
+                    onClick={() => setExpandedCard(expandedCard === 1 ? null : 1)}
+                />
+                <Card
+                    image={learnPhoto}
+                    title="What Will Be Taught?"
+                    description="Together we will guide each other through God's life changing truths. Remember, our messages are Biblical.  Each week we take what the Bible says and learn how to apply it to our real life issues and questions and purpose.  God's Word grows us and feeds our souls, God gives you something through His Word on Sunday that you can start applying on Sunday."
+                    isExpanded={expandedCard === 2}
+                    onClick={() => setExpandedCard(expandedCard === 2 ? null : 2)}
+                />
+                <Card
+                    image={clockPhoto}
+                    title="How Long Is Service?"
+                    description="We meet every Sunday at 10:00 AM.  We are located at 1000 N. 16th St. in Grand Junction, CO.  We would love to have you join us!"
+                    isExpanded={expandedCard === 3}
+                    onClick={() => setExpandedCard(expandedCard === 3 ? null : 3)}
+                />
+                <Card
+                    image={coffeePhoto}
+                    title="After Service?"
+                    description="We provide great coffee and refreshments after each service in our fellowship hall for your enjoyment and relationship building we value our children and they have fun during this time just being kids. This a huge part of who we are as a church, and we would be blessed to connect join in the joy."
+                    isExpanded={expandedCard === 4}
+                    onClick={() => setExpandedCard(expandedCard === 4 ? null : 4)}
+                />
+            </div>
+        </>
     );
 }
 
-function Card(props) {
-    const [toggle, setToggle] = useState(false);
-
+function Card({ image, title, description, isExpanded, onClick }) {
     return (
         <motion.div
-        whileHover={{ scale: 1.05 }}
-
-        className="card" onClick={() => setToggle(!toggle)}>
-            <img src={props.image} alt="Bible Photo" />
-            <h2>{props.title}</h2>
+            whileHover={{ scale: 1.05 }}
+            className="card"
+            onClick={onClick}
+        >
+            <img src={image} alt="Bible Photo" />
+            <h2>{title}</h2>
             <div className="toggleBox">
-                <p>{toggle ? "-" : "+"}</p>
+                <p>{isExpanded ? "-" : "+"}</p>
             </div>
-            <div className={`cardContent ${toggle ? 'show' : ''}`}>
-                <p>{props.description}</p>
+            <div className={`cardContent ${isExpanded ? 'show' : ''}`}>
+                <p>{description}</p>
             </div>
         </motion.div>
     );
