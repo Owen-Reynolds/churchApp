5875674b61180a972dae9a9dd9bde6dfd7b3c6cc
Nolan Kern <nolankern24@gmail.com>
2025-02-10 11:55:43 -0500

    18

--- frontend/church-app/src/pages/Staff/Staff.jsx
+++ frontend/church-app/src/pages/Staff/Staff.jsx
@@ -1,14 +1,19 @@
 import "./staff.css";
 import heartrate from '../../assets/heartRateVid.mp4';
-
+import background from '../../assets/staffBottom.svg';
 
 export default function AboutUs() {
     return(
     <>
     <body>
-    <div className="header">
-         <video src ={heartrate} autoPlay loop muted/>
+    <div className="staffHeader">
+         <video className="heartvid" src ={heartrate} autoPlay loop muted/>
         <h1>Meet the Team.</h1>
+    </div>
+    <div className="mainStaffContent">
+       <div className="cards">
+        
+       </div>
     </div>
     </body>
     </>
