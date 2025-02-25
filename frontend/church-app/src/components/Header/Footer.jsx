87b8684c315924a39354f3e09f59481e6151dc68
Nolan Kern <nolankern24@gmail.com>
2025-02-25 11:51:06 -0500

    24

--- frontend/church-app/src/components/Header/Footer.jsx
+++ frontend/church-app/src/components/Header/Footer.jsx
@@ -4,9 +4,10 @@ import './Footer.css';
 import { Link } from 'react-router-dom';
 
 import crossImage from '../../assets/homePageAssets/vitalitylogowhite.png';
-import fbLogo from '../../assets/facebooklogo.png';
-import ytLogo from '../../assets/ytlogo.png';
-import igLogo from '../../assets/instagramlogo.png';
+import fbLogo from '../../assets/footerAssets/facebookLogo.png';
+import ytLogo from '../../assets/footerAssets/ytLogo.png';
+import igLogo from '../../assets/footerAssets/instagramLogo.png';
+import liLogo from '../../assets/footerAssets/linkedin.png';
 
 export default function Footer() {
     return (
@@ -42,6 +43,15 @@ export default function Footer() {
                 </div>
                 <div className="footer-bottom-designers">
                 <p>Website by:</p>
+                <a href="https://www.linkedin.com/in/nolan-kern25/" target="_blank" rel="linkednInNolan">
+                <img src={liLogo} alt="Nolan" />
+                </a>
+                <a href="https://www.linkedin.com/in/owen-reynolds-b8870a293/" target="_blank" rel="linkednInOwen">
+                <img src={liLogo} alt="Owen" />
+                </a>
+                <a href="https://www.linkedin.com/in/enoch-oye-110800338/" target="_blank" rel="linkednInEnoch">
+                <img src={liLogo} alt="Enoch" />
+                </a>
                 </div>
             </div>
         </footer>
