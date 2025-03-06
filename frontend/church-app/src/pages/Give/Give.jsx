3f651bc3cd3f27e5e49a28ccbb8447dc1406e5b1
Nolan Kern <nolankern24@gmail.com>
2025-03-06 13:16:17 -0500

    27

--- frontend/church-app/src/pages/Give/Give.jsx
+++ frontend/church-app/src/pages/Give/Give.jsx
@@ -3,7 +3,13 @@ import './give.css';
 export default function Give() {
     return(
     <>
-        <h1>Give Page</h1>
+        <div className="give-container">
+            <h1>Support Our Mission</h1>
+            <p>Your generous donations help us continue our work and support our community.</p>
+            <a href="https://www.paypal.com/donate/?hosted_button_id=Q4SNR47UHK2DE" target="_blank" rel="noopener noreferrer">
+      <button className="donate-button">Donate</button>
+            </a>
+        </div>
     </>
     );
 }
