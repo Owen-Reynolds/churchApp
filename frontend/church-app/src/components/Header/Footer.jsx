e2c4baba03640d0b0ea6af79401058d5717e6433
Nolan Kern <nolankern24@gmail.com>
2025-03-12 10:23:44 -0400

    29

--- frontend/church-app/src/components/Header/Footer.jsx
+++ frontend/church-app/src/components/Header/Footer.jsx
@@ -45,7 +45,8 @@ export default function Footer() {
                 </div>
                 <div className="footer-bottom-designers">
                 <p>Website by:</p>
-                <a href="https://www.linkedin.com/in/nolan-kern25/" target="_blank" rel="linkednInNolan">
+                
+               { /*<a href="https://www.linkedin.com/in/nolan-kern25/" target="_blank" rel="linkednInNolan">
                 <img src={liLogo} alt="Nolan" />
                 </a>
                 <a href="https://www.linkedin.com/in/owen-reynolds-b8870a293/" target="_blank" rel="linkednInOwen">
@@ -53,7 +54,7 @@ export default function Footer() {
                 </a>
                 <a href="https://www.linkedin.com/in/enoch-oye-110800338/" target="_blank" rel="linkednInEnoch">
                 <img src={liLogo} alt="Enoch" />
-                </a>
+                </a>*/}
                 </div>
             </div>
         </footer>
