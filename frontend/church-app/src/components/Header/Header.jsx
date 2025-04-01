a7769f902ee62063ddcc5ad4af4bb34baa5b3e9c
OwenxOwen <oreynold@ashland.edu>
2025-04-01 11:14:25 -0400

    Started Header CSS

--- frontend/church-app/src/components/Header/Header.jsx
+++ frontend/church-app/src/components/Header/Header.jsx
@@ -4,31 +4,39 @@ import './MobileHeader.css';
 
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import {useState} from 'react';
 
 import crossImage from '../../assets/vitalitylogoblue.png';
 
 export default function Header() {
+    const [navOpen, setNavOpen] = useState(false);
     return(
     <>
     <header>
         <div className="logoHeader">
             <Link to='/'>
-            <motion.img 
-            whileHover={{ scale: 1.1}}
-            whileTap={{ scale: 0.9 }}
-            src={crossImage} alt="" 
-            />
+                <motion.img 
+                    whileHover={{ scale: 1.1}}
+                    whileTap={{ scale: 0.9 }}
+                    src={crossImage} alt="" 
+                />
             </Link>
         </div>
-        <ul>
-            <li><Link to='/AboutUs'>About Us</Link></li>
-            <li><Link to='/SundayMornings'>Sunday Mornings</Link></li>
-            <li><Link to='/Staff'>Staff</Link></li>
-            <li><Link to='/Youth'>Youth</Link></li>
-            <li><Link to='/ChristianCadets'>Christian Cadets</Link></li>
-            <li><Link to='/Connect'>Connect</Link></li>
-            <li><Link to='/Give'>Give</Link></li>
-        </ul>
+        <div className="openButtonHeader">
+            <span onClick={() => setNavOpen(true)}>Open</span>
+        </div>
+        <div className="mobileOverlay" style={{width: navOpen ? "100%" : "0%"}}>
+            <a href="#" className="closeBtn" onClick={() => setNavOpen(false)}>&times;</a>
+            <ul>
+                <li><Link to='/AboutUs' onClick={() => setNavOpen(false)}>About Us</Link></li>
+                <li><Link to='/SundayMornings' onClick={() => setNavOpen(false)}>Sunday Mornings</Link></li>
+                <li><Link to='/Staff' onClick={() => setNavOpen(false)}>Staff</Link></li>
+                <li><Link to='/Youth' onClick={() => setNavOpen(false)}>Youth</Link></li>
+                <li><Link to='/ChristianCadets' onClick={() => setNavOpen(false)}>Christian Cadets</Link></li>
+                <li><Link to='/Connect' onClick={() => setNavOpen(false)}>Connect</Link></li>
+                <li><Link to='/Give' onClick={() => setNavOpen(false)}>Give</Link></li>
+            </ul>
+        </div>
     </header>
     </>
     );
