b30c74f5f885891723c4b41e09a40879c1b9e6eb
OwenxOwen <oreynold@ashland.edu>
2025-04-01 13:17:29 -0400

    Footer Mobile Changes

--- frontend/church-app/src/components/Footer/Footer.jsx
+++ frontend/church-app/src/components/Footer/Footer.jsx
@@ -39,33 +39,31 @@ export default function Footer() {
                 <div className="footer-social">
                     <h2>Socials</h2>
                     <div className = "social-grid">
-                    <a href="https://www.youtube.com/@vitalitychurch5132" target="_blank" rel="youTube"><img src = {ytLogo} alt="youTube"/></a>
-                    <a href="https://www.instagram.com/vitality_church/" target="_blank" rel="instagram"><img src = {igLogo} alt="instagram"/></a>
-                    <a href="https://www.facebook.com/profile.php?id=61566505584528" target="_blank" rel="faceBook"><img src = {fbLogo} alt="faceBook"/></a>
+                        <a href="https://www.youtube.com/@vitalitychurch5132" target="_blank" rel="youTube"><img src = {ytLogo} alt="youTube"/></a>
+                        <a href="https://www.instagram.com/vitality_church/" target="_blank" rel="instagram"><img src = {igLogo} alt="instagram"/></a>
+                        <a href="https://www.facebook.com/profile.php?id=61566505584528" target="_blank" rel="faceBook"><img src = {fbLogo} alt="faceBook"/></a>
                     </div>
                 </div>
 
                 <div className="map-responsive">
                     <h2 className="footerLocationHeading">Location</h2>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2978.3178389291693!2d-81.30121112410387!3d41.71365987126017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8831a5580607348d%3A0xbadb134bb5a527b3!2sLaMalfa%20Centre%20Hotel%20and%20Suites!5e0!3m2!1sen!2sus!4v1739983511197!5m2!1sen!2sus" 
-                 allowfullscreen="" 
-                loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Responsive Google Map"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2978.3178389291693!2d-81.30121112410387!3d41.71365987126017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8831a5580607348d%3A0xbadb134bb5a527b3!2sLaMalfa%20Centre%20Hotel%20and%20Suites!5e0!3m2!1sen!2sus!4v1739983511197!5m2!1sen!2sus" 
+                        allowfullscreen="" 
+                        loading="lazy" referrerPolicy="no-referrer-when-downgrade" title="Responsive Google Map">     
+                    </iframe>
                 </div>
             </div>
             <div className="footer-bottom">
                 <div className="footer-bottom-img">
-                <img src = {crossImage} alt=""></img>
+                    <img src = {crossImage} alt=""></img>
                 </div>
                 <div className="footer-bottom-text">
-                <p> Vitality Church | Est. 2014</p>
+                    <p> Vitality Church | Est. 2014</p>
                 </div>
                 <div className="footer-bottom-designers">
-                <Link to ="/WebDesigners">
+                    <Link to ="/WebDesigners">
                         <button className="designersButton">Web Designers</button>
-                        </Link>
-                {/*<p>Website by:</p>*/}
-                
-               
+                    </Link>
                 </div>
             </div>
         </footer>
