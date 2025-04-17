765f1d9f0530ab7c04ebf2aa89a829f58447cb3d
Nolan Kern <nolankern24@gmail.com>
2025-04-17 18:22:10 -0400

    favicon + staff update

--- frontend/church-app/src/pages/Staff/Staff.jsx
+++ frontend/church-app/src/pages/Staff/Staff.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Staff.css';
 import './MobileStaff.css';
 import heartrate from '../../assets/staffAssets/heartRateVid.mp4';
-import pastorDave from '../../assets/staffAssets/pastorDave.jpg';
+import pastorDave from '../../assets/staffAssets/davidKern.jpg';
 import bobAnderson from '../../assets/staffAssets/bobAnderson.jpg';
 import dorothyHarvey from '../../assets/staffAssets/dorothyHarvey.jpg';
 import timMcNamee from '../../assets/staffAssets/timMcNamee.jpg';
